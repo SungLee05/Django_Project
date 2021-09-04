dd3f64fb3ab711de8d9cef7c397b6d789789f161
Sung <sungyonglee414@gmail.com>
2021-09-03 18:03:06 -0700

    search placeholders added

--- listings/views.py
+++ listings/views.py
@@ -64,5 +64,6 @@ def search(request):
         'bedroom_choices': bedroom_choices,
         'state_choices': state_choices,
         'listings': queryset_list,
+        'values': request.GET
     }
     return render(request, 'listings/search.html', context)
