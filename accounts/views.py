2742caa59067306aa61f414de6ca9aa7c1a83408
Sung <sungyonglee414@gmail.com>
2021-09-07 19:22:26 -0700

    initial if conditions added to register to check if username and email exist

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
+        # check if password match
+        if password == password2:
+            # check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exists.')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already exists.')
+                    return redirect('register')
+                else:
+                    # looks good
+                    pass
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
