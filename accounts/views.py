53cd4ff492a5723711e1953fd34692b6afecb3a2
Sung <sungyonglee414@gmail.com>
2021-09-08 21:31:36 -0700

    registration method complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -24,8 +24,14 @@ def register(request):
                     messages.error(request, 'Email already exists.')
                     return redirect('register')
                 else:
-                    # looks good
-                    pass
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    # Login after registering
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged in.')
+                    # return redirect('index')
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('login')
         else:
             messages.error(request, 'Passwords do not match')
             return redirect('register')
